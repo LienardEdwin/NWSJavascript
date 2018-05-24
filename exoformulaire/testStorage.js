c9642f1c2fccf5d7a364c590f641775c57109809
Edwin LIENARD <edwin.lienard@outlook.fr>
2018-05-24 11:23:37 +0200

    exercice localstorage/exo test

---
+++ exoformulaire/testStorage.js
@@ -0,0 +1,15 @@
+function testStorage(){
+    let nom = document.getElementById('nom').value;
+    let prenom = document.getElementById('prenom').value;
+    let mail = document.getElementById('mail').value;
+    let pwd = document.getElementById('mdp').value;
+
+    sessionStorage.setItem("Nom", nom);
+    sessionStorage.setItem("Prenom", prenom);
+    sessionStorage.setItem("email", mail);
+    sessionStorage.setItem("Mot de passe", pwd);
+
+    let toto = sessionStorage.getItem("Nom");
+
+
+}
